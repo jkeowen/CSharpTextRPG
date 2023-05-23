6656d8445cafff2e9cdd6b913cf2b4cf9e9d3a62
Justin Keowen <jkeowen@gmail.com>
2023-05-23 09:25:59 -0500

    first commit

---
+++ TextRPG/Creature.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public class Creature
+    {
+        public int ID { get; }
+        public string Species { get; }
+        public int BaseHitPoints { get; }
+        public int CurrentHitPoints { get; set; }
+        
+        private static int idSeed = 0;
+
+
+        public Creature(string species, int basehitPoints)
+        {
+            Species = species;
+            ID = idSeed++;
+            BaseHitPoints = basehitPoints;
+            CurrentHitPoints = basehitPoints;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if(damage > 0)
+            {
+                if(damage > CurrentHitPoints)
+                {
+                    CurrentHitPoints = 0;
+                }
+                else
+                {
+                    CurrentHitPoints -= damage;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Damage cannot be negative");
+            }
+        }
+
+       /* public void Heal(int healAmount)
+        {
+            if(healAmount < HitPoints)
+            {
+                HitPoints += healAmount;
+            }
+        }*/
+
+       
+    }
+}
---
+++ TextRPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextRPG 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var creature1 = new Creature("test", 59);
+            Console.WriteLine(creature1.CurrentHitPoints);
+            creature1.TakeDamage(-6);
+            Console.WriteLine(creature1.CurrentHitPoints);
+           // creature1.Heal(100);
+            //Console.WriteLine(creature1.HitPoints);
+        }
+    }
+}
